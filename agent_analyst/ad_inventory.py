21b05ddfd983dce32871697fded2dbf123425a34
shironaganegi <sai3desuyo@gmail.com>
2026-02-05 18:46:34 +0900

    Update ad_inventory.py

--- agent_analyst/ad_inventory.py
+++ agent_analyst/ad_inventory.py
@@ -1,4 +1,74 @@
 AD_CAMPAIGNS = [
+    # --- XServer VPS (開発者・ゲーマー向け) ---
+    {
+        "name": "XServer VPS (AI開発)",
+        "html": """
+[DifyでAIアプリを開発するなら『XServer VPS』](https://px.a8.net/svt/ejp?a8mat=4AX40H+4EYRAQ+CO4+25HS3M)
+![](https://www15.a8.net/0.gif?a8mat=4AX40H+4EYRAQ+CO4+25HS3M)
+"""
+    },
+    {
+        "name": "XServer VPS (Minecraft)",
+        "html": """
+Minecraftマルチプレイするなら[『XServer VPS』](https://px.a8.net/svt/ejp?a8mat=4AX40H+4EYRAQ+CO4+25FUNM)
+![](https://www19.a8.net/0.gif?a8mat=4AX40H+4EYRAQ+CO4+25FUNM)
+"""
+    },
+    {
+        "name": "XServer VPS (汎用)",
+        "html": """
+[国内シェアNo.1のエックスサーバーが提供するVPSサーバー『XServer VPS』](https://px.a8.net/svt/ejp?a8mat=4AX40H+4EYRAQ+CO4+25F7IA)
+![](https://www11.a8.net/0.gif?a8mat=4AX40H+4EYRAQ+CO4+25F7IA)
+"""
+    },
+
+    # --- ConoHa WING (ブロガー・AI利用者向け) ---
+    {
+        "name": "ConoHa WING (AIブログツール)",
+        "html": """
+[ConoHa Pencil でブログ運営を超効率化](https://px.a8.net/svt/ejp?a8mat=4AX40H+48EZN6+50+5SXFLU)
+![](https://www12.a8.net/0.gif?a8mat=4AX40H+48EZN6+50+5SXFLU)
+"""
+    },
+    {
+        "name": "ConoHa WING (コスパ抜群1時間2円)",
+        "html": """
+[1時間2円から、国内最速・高性能レンタルサーバー【ConoHa WING】](https://px.a8.net/svt/ejp?a8mat=4AX40H+48EZN6+50+5SJACI)
+![](https://www17.a8.net/0.gif?a8mat=4AX40H+48EZN6+50+5SJACI)
+"""
+    },
+    {
+        "name": "ConoHa WING (国内最速・安定)",
+        "html": """
+[国内最速・高安定の高性能レンタルサーバー【ConoHa WING】](https://px.a8.net/svt/ejp?a8mat=4AX40H+48EZN6+50+5SI01U)
+![](https://www11.a8.net/0.gif?a8mat=4AX40H+48EZN6+50+5SI01U)
+"""
+    },
+
+    # --- お名前.com (ドメイン取得) ---
+    {
+        "name": "お名前.com (キャッチー)",
+        "html": """
+[世界にたった一つ、あなただけのドメインを登録しよう！](https://px.a8.net/svt/ejp?a8mat=4AX38F+M125U+50+2HE0RM)
+![](https://www15.a8.net/0.gif?a8mat=4AX38F+M125U+50+2HE0RM)
+"""
+    },
+    {
+        "name": "お名前.com (煽り系)",
+        "html": """
+[【早い者勝ち！】 あなたのお名前、残ってる？](https://px.a8.net/svt/ejp?a8mat=4AX38F+M125U+50+2HCY6Q)
+![](https://www15.a8.net/0.gif?a8mat=4AX38F+M125U+50+2HCY6Q)
+"""
+    },
+    {
+        "name": "お名前.com (シンプル)",
+        "html": """
+[お名前.com](https://px.a8.net/svt/ejp?a8mat=4AX38F+M125U+50+2HHVNM)
+![](https://www11.a8.net/0.gif?a8mat=4AX38F+M125U+50+2HHVNM)
+"""
+    },
+
+    # --- フォールバック (楽天) ---
     {
         "name": "スッキリわかるPython入門 第2版",
         "html": """
